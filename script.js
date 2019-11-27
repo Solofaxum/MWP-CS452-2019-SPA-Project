f7fccc2673ef8a47fcc5629a25f78fc612ece5dd
Solofaxum <solofaxum6@gmail.com>
2019-11-27 01:36:52 -0600

    First Day Report of login part

--- script.js
+++ script.js
@@ -2,57 +2,58 @@
 
 window.onload = function singlePageApp() {
 
-    // let GEOLOCATION_API_KEY;
+   
     // let toke;
     // letAnimationId;
     // const outlet = document.querySelector('#outlet');
-    GEOLOCATION_API_KEY = 'GsbAzLMtt611mbx5obQx05Pumn8wJeGJ'
-    let outlate = document.querySelector('#outlate')
-    let animationId;
 
+    // let outlate = document.querySelector('#outlate');
+    // let animationId;
+    // let loged = false;
+    let myLocation = "KEYO3z20eyf8ZaJpI88gkhQQbhe2MpIo1lC";
 
-
-    const logIn = `
+    const logInPage = `
        <h1> Please login</h1>
        User name: <input type="text" id="userName"><br><br>
        Password: <input type= "text" id= "password"><br><br>
-       <input type="button" value="Login" onclick="loginDisplay()">`
+       <input type="button" id="Login1" value="LogIn"">`
 
     const myAnimation = `
-    <div id= "animation"> welcome to liveliness </div>
-    <textarea id = animationView"  rows="10" cols="30" stayle = "font-size:10px"> </textarea><br>
+    <h1 id= "status"> "start" </h1>
+    <div id= "animation"> welcome to Animation Stage </div>
+    <textarea id = animationView"  rows="30" cols="50" stayle = "font-size:10px"> </textarea><br>
     <input type="button" value="Refresh Animation" onclick="refresh()">
-    <input type="button" value="Logout" onclick="logout()">`
+    <input type="button" id="Logout" value="LogOut">`
 
-    const divDisplay = document.querySelector("#outlet");
-    divDisplay.innerHTML = logIn;
+    const myDiv = document.querySelector("#outlet");
+    myDiv.innerHTML = logInPage;
 
-    renderPage({ page: 'login' }, '/');
-    function renderPage(state, url) {
-        if (state.page === "login") {
-            outlate.innerHTML = logIn;
-            history.pushstate(state, null, url);
-            addListener(state);
-        }
-        if (state.page === animation) {
-            outlate.innerHTML = myAnimation;
-            history.pushstate(state, null, url);
-            addListener(state);
-            fetchAnimation();
-            fetchaddress();
+    let log = document.querySelector("#Login1");
+    log.addEventListener("click", login);
+
+    function login() {
+        myDiv.innerHTML = myAnimation;
+        Event();
+
+    }
+
+    function logOutFunc() {
+        myDiv.innerHTML = logInPage;
+    }
+
+    function Event() {
+        const logout = document.querySelector("#Logout");
+        logout.addEventListener("click", logOutFunc);
+    }
+
+    async function fetchLocation() {
+        navigator.geolocation.getCurrentPosition(success, fail)
+        async function success(position) {
+
         }
     }
-    function addListener({ page }) {
-        if (page === 'login') {
-            document.querySelector('button').addEventListener('click, loginFunction');
-        }
-        if (page === 'animation') {
-            document.querySelector('#refresh').addEventListener('click, fitchAnimation');
-            document.querySelector('#logout').addEventListener('click, fitchAnimation');
-        }
-    }
-function loginFunction(
 
 }
+
 
 
