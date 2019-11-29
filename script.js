200c768a7a964fe61ed769f4f88cc96d59252f4b
Solofaxum <solofaxum6@gmail.com>
2019-11-28 18:35:28 -0600

    Third day status report

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 
 
 window.onload = function singlePageApp() {
-
-
+    let picAni = `
+    
+     o
+    /#\
+    _|_
+   =====
+    \o/
+     #
+   _/ \_
+    `;
+    let framesLength;
     let tokenn, animationId;
     let myLocation = "O3z20eyf8ZaJpI88gkhQQbhe2MpIo1lC";
 
     const logInPage = `
+    <link rel="stylesheet" href="css.css">
        <h1> Please login</h1>
        User name: <input type="text" id="userName" value="mwp"><br><br>
        Password: <input type= "text" id= "password" value= "123"><br><br>
        <input type="button" id="Login1" value="LogIn"">`
 
     const myAnimation = `
-    
+    <link rel="stylesheet" href="css.css">
     <h1 id= "animation"> </h1>
-    <div> Well Come to The World of Animation </div>
+    <div class ="animate-page"> The World of Animation: Click Refresh and Enjoy! </div>
     <textarea id = "animationView"  rows="30" cols="50" stayle = "font-size:10px"></textarea><br>
     <button type="button" id="animationn"  >Refresh</button>
     <input type="button" id="Logg" value="LogOut">`
@@ -25,9 +35,9 @@ window.onload = function singlePageApp() {
 
     let log = document.querySelector("#Login1");
     log.addEventListener("click", login);
-    
+
     function login() {
-        //alert("you are loged in")
+
         myDiv.innerHTML = myAnimation;
         logOutFunc();
         fetchLocation()
@@ -35,8 +45,8 @@ window.onload = function singlePageApp() {
         fitchAnimation();
     }
 
-    function logBackFunc() {
-        //alert("your are out");
+    function logFunc() {
+
         myDiv.innerHTML = logInPage;
         animationss();
     }
@@ -45,32 +55,26 @@ window.onload = function singlePageApp() {
         const frames = picAni.split('=====')
         const framesLength = frames.length;
         let currentFrame = 0;
-        
-         setInterval(() => {
-            document.querySelector('#animationView').value = frames[currentFrame];
-            currentFrame++;
-            if (currentFrame === framesLength)
-                currentFrame = 0;
-        }, 200);
+
         const logout = document.querySelector("#Logg");
-        logout.addEventListener("click", logBackFunc);
+        logout.addEventListener("click", logFunc);
 
-             
-       document.getElementById("animationn").addEventListener("click",function(){
-           console.log(fitchAnimation());
-        fitchAnimation().then(reso=>{
-            const frames = reso.split('=====\n')
-            const framesLength = frames.length;
-            let currentFrame = 0;
-            animationId = setInterval(() => {
-                document.querySelector('#animationView').value = frames[currentFrame];
-                currentFrame++;
-                if (currentFrame === framesLength)
-                    currentFrame = 0;
-            }, 200)
+
+        document.getElementById("animationn").addEventListener("click", function () {
+            console.log(fitchAnimation());
+            fitchAnimation().then(reso => {
+                const frames = reso.split('=====\n')
+                const framesLength = frames.length;
+                let currentFrame = 0;
+                animationId = setInterval(() => {
+                    document.querySelector('#animationView').value = frames[currentFrame];
+                    currentFrame++;
+                    if (currentFrame === framesLength)
+                        currentFrame = 0;
+                }, 200)
+            });
+
         });
-
-       });
 
     }
 
@@ -83,8 +87,6 @@ window.onload = function singlePageApp() {
             const Longitude = position.coords.longitude;
             const Latitude = position.coords.latitude;
 
-            // console.log('Longitude:' + Longitude);
-            // console.log('Latitude:' + Latitude);
 
             let response = await fetch(`http://open.mapquestapi.com/geocoding/v1/reverse?key=${myLocation}&location=${Latitude},${Longitude}&includeRoadMetadata=true&includeNearestIntersection=true`)
             response = await response.json()
@@ -119,25 +121,27 @@ window.onload = function singlePageApp() {
             });
         const respData = await response.json();
         const myToken = respData.token;
-      //  console.log(myToken);
+
     }
 
-    let picAni=`
-    
-     o
-    /#\
-    _|_
-   =====
-    \o/
-     #
-   _/ \_
-    `;
 
     tokenn = `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtd2EiLCJpc3N1ZWRBdCI6IjIwMTktMTEtMjciLCJ1c2VybmFtZSI6Im13cCJ9.U9ciwh5lcPwFlJdxhNQkeiMc7AMYAnawfKNidw8CNDpTIUjNBL_EtDqkXG4qGOF8H_Ve1S2Gg2PwmCYOkfgmWA`
 
-    
+    async function fitchAnimation() {
+        if (animationId) clearInterval(animationId);
+        const response = await fetch('http://mumstudents.org/api/animation',
+            {
+                method: "GET",
+                headers: {
+                    'Authorization': `Bearer ${tokenn}`
+                }
+            })
 
+        const animation = await response.text();
+        console.log(animation);
+        return animation;
 
+    }
 
 }
 
